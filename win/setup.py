2aac25708e2914386bbb0d4b2bac19a6aac88b7e
pohmelie <multisosnooley@gmail.com>
2014-08-11 11:11:54 +0400

    added 3.4 version for windows

--- win/setup.py
+++ win/setup.py
@@ -6,7 +6,7 @@ setup(
     version = "4.08",
     ext_modules = [Extension(
         "_WDMTMKv2",
-        ["WDMTMKv2.i","WDMTMKv2.cpp"],
+        ["WDMTMKv2.i", "WDMTMKv2.cpp"],
         swig_opts=['-py3', '-c++', '-module','WDMTMKv2'],
     )]
 )
